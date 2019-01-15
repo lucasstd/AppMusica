775509e8dd14bd0d1196dc0743ff34c97f58d248
Lucas Staudt <lucas@10-41-40-42.0101.epoa.br.iosda.org>
2019-01-15 16:21:08 -0200

    Initial work

--- AppMusica/ViewController.swift
+++ AppMusica/ViewController.swift
@@ -1,20 +1,42 @@
 //
 //  ViewController.swift
-//  AppMusica
+//  FFFinalChallenge
 //
-//  Created by Lucas de Oliveira Staudt on 15/01/19.
+//  Created by Lucas de Oliveira Staudt on 14/01/19.
 //  Copyright © 2019 apple. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var listToPurchase: UITableView!
 
+//    var list = ShoppingList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
-
-
 }
 
+//extension ViewController: UITableViewDataSource, UITableViewDelegate {
+//    
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//        return list.items.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        // cell.textLabel?.text = indexPath.row.description
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath)
+//    }
+//}
