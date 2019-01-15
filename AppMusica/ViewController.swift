d0bfb6b74334fb5504f6d16c87fdff7c8aeb4548
Lucas Staudt <lucas@10-41-49-20.0200.epoa.br.iosda.org>
2019-01-15 16:00:36 -0200

    Initial Commit

---
+++ AppMusica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppMusica
+//
+//  Created by Lucas de Oliveira Staudt on 15/01/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
